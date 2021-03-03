38e89a9d7847ba36a8f3bbec29d9a1dc061208e9
solo-bog <bsoloxa@gmail.com>
2021-03-03 08:59:57 +0200

    add authorization check and make links to users profiles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import s from './Header.module.css'
-const Header = () => {
+import {NavLink} from "react-router-dom";
+const Header = (props) => {
     return(
         <header className={s.header}>
             <img src="https://www.veeam.com/content/dam/veeam/global/veeam-graphics/veeam_logo_white-500.png.web.1280.1280.png?ck=1572622163865&ck=1572622163865" alt="logo"/>
+            <div className={s.loginBlock}>
+                {props.isAuth ? 'free': <NavLink to={'/login'}>Login</NavLink>}
+
+            </div>
         </header>
     );
 }
